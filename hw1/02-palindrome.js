c87a36f7a83d11fa28a83b6601436c2bacb1ddb0
Nathan Moes <nathanmoes7@gmail.com>
2023-10-01 13:43:04 -0700

    airbnb stuff

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -5,19 +5,19 @@ elem.addEventListener('input', handleInput);
 function handleInput(event) {
   const input = event.target.value;
   const inputLength = input.length;
-  if (inputLength == 0) {
+  if (inputLength === 0) {
     result.textContent = '';
     return;
   }
-  if (input[0] == '-') {
+  if (input[0] === '-') {
     result.textContent = 'Only positive numbers can be palindromes!';
     result.classList.remove('text-success');
     result.classList.add('text-danger');
     return;
   }
   for (let i = 0; i < inputLength / 2; i++) {
-    let left = input[i];
-    let right = input[inputLength - 1 - i];
+    const left = input[i];
+    const right = input[inputLength - 1 - i];
     if (left !== right) {
       result.textContent = 'It is not a palindrome!';
       result.classList.remove('text-success');
