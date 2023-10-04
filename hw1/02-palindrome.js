43b8faca18d4264850b8dcc58a9ebf0f49746851
NathanMoes <nathanmoes7@gmail.com>
2023-10-04 13:05:22 -0700

    added in nan check. Tho not needed

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,31 +1,37 @@
-const elem = document.querySelector('input');
-const result = document.querySelector('#result');
+const elem = document.querySelector("input");
+const result = document.querySelector("#result");
 
-elem.addEventListener('input', handleInput);
+elem.addEventListener("input", handleInput);
 function handleInput(event) {
   const input = event.target.value;
   const inputLength = input.length;
   if (inputLength === 0) {
-    result.textContent = '';
+    result.textContent = "";
     return;
   }
-  if (input[0] === '-') {
-    result.textContent = 'Only positive numbers can be palindromes!';
-    result.classList.remove('text-success');
-    result.classList.add('text-danger');
+  if (isNaN(input)) {
+    result.textContent = "Please enter a number!";
+    result.classList.remove("text-success");
+    result.classList.add("text-danger");
+    return;
+  }
+  if (input[0] === "-") {
+    result.textContent = "Only positive numbers can be palindromes!";
+    result.classList.remove("text-success");
+    result.classList.add("text-danger");
     return;
   }
   for (let i = 0; i < inputLength / 2; i++) {
     const left = input[i];
     const right = input[inputLength - 1 - i];
     if (left !== right) {
-      result.textContent = 'It is not a palindrome!';
-      result.classList.remove('text-success');
-      result.classList.add('text-danger');
+      result.textContent = "It is not a palindrome!";
+      result.classList.remove("text-success");
+      result.classList.add("text-danger");
       return;
     }
   }
-  result.textContent = 'It is indeed a palindrome!';
-  result.classList.remove('text-danger');
-  result.classList.add('text-success');
+  result.textContent = "It is indeed a palindrome!";
+  result.classList.remove("text-danger");
+  result.classList.add("text-success");
 }
