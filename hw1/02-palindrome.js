54af8a2ea21e8f04677b9d4d307282b383b6cb70
Nathan Moes <nathanmoes7@gmail.com>
2023-10-08 10:33:14 -0700

    styling functions added

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,6 +1,16 @@
 const elem = document.querySelector('input');
 const result = document.getElementById('result');
 
+function validInuptStyles() {
+  result.classList.remove('text-danger');
+  result.classList.add('text-success');
+}
+
+function invalidInputStyles() {
+  result.classList.remove('text-success');
+  result.classList.add('text-danger');
+}
+
 elem.addEventListener('input', handleInput);
 function handleInput(event) {
   const input = event.target.value;
@@ -11,14 +21,12 @@ function handleInput(event) {
   }
   if (isNaN(input)) {
     result.textContent = 'Please enter a number!';
-    result.classList.remove('text-success');
-    result.classList.add('text-danger');
+    invalidInputStyles();
     return;
   }
   if (input[0] === '-') {
     result.textContent = 'Only positive numbers can be palindromes!';
-    result.classList.remove('text-success');
-    result.classList.add('text-danger');
+    invalidInputStyles();
     return;
   }
   for (let i = 0; i < inputLength / 2; i++) {
@@ -26,12 +34,10 @@ function handleInput(event) {
     const right = input[inputLength - 1 - i];
     if (left !== right) {
       result.textContent = 'It is not a palindrome!';
-      result.classList.remove('text-success');
-      result.classList.add('text-danger');
+      invalidInputStyles();
       return;
     }
   }
   result.textContent = 'It is indeed a palindrome!';
-  result.classList.remove('text-danger');
-  result.classList.add('text-success');
+  validInuptStyles();
 }
