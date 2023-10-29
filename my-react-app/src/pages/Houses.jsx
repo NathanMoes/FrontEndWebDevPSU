204382046b1ef5012fc370181148761f31337098
Nathan Moes <nathanmoes7@gmail.com>
2023-10-28 19:12:30 -0700

    trying to make chart not too large

--- my-react-app/src/pages/Houses.jsx
+++ my-react-app/src/pages/Houses.jsx
@@ -2,9 +2,11 @@ import HousesDoughnut from '../components/HousesDoughnut';
 
 const Houses = () => {
   return (
-    <div>
-      <h1>Houses</h1>
-      <HousesDoughnut />
+    <div className="d-flex flex-column justify-content-center align-content-center">
+      <h1 className="text-center">Houses</h1>
+      <div className="w-75 mx-auto">
+        <HousesDoughnut />
+      </div>
     </div>
   );
 };
