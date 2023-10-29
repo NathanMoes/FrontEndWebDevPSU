76bf59da8e0f0c1e74b308aa5fbccc464649a751
Nathan Moes <nathanmoes7@gmail.com>
2023-10-28 17:20:08 -0700

    chart seems to be more or less working

--- hw3/02-charts.js
+++ hw3/02-charts.js
@@ -1,3 +1,29 @@
+const moreColors = [
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+  getRandomColor(),
+];
+
 const backgroundColors = [
   'rgba(54, 162, 235, 0.8)',
   'rgba(255, 206, 86, 0.8)',
@@ -10,7 +36,21 @@ const backgroundColors = [
   'rgba(40, 159, 64, 0.8)',
   'rgba(210, 199, 199, 0.8)',
   'rgba(78, 52, 199, 0.8)',
+  ...moreColors,
 ];
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  let a = Math.random();
+  if (a > 0.8) {
+    // make sure the alpha is dimmed
+    a -= 0.5;
+  }
+  const color = `rgba(${r}, ${g}, ${b}, ${a})`;
+  return color;
+}
 
 const borderColors = [
   'rgba(54, 162, 235, 1)',
@@ -24,22 +64,89 @@ const borderColors = [
   'rgba(40, 159, 64, 1)',
   'rgba(210, 199, 199, 1)',
   'rgba(78, 52, 199, 1)',
+  ...moreColors,
 ];
+
+function capitalizeHouseNames(str) {
+  return str
+    .split(' ')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+}
+
+function standardizeHouseName(name) {
+  if (!name) return name;
+
+  name = name.trim().toLowerCase();
+
+  if (name.includes('stark')) {
+    return 'House Stark';
+  } else if (name.includes('lannister') || name.includes('lanister')) {
+    return 'House Lannister';
+  } else if (
+    name.includes('targaryen') ||
+    name.includes('targaryn') ||
+    name.includes('targaryan')
+  ) {
+    return 'House Targaryen';
+  } else if (name.includes('baratheon')) {
+    return 'House Baratheon';
+  } else if (name.includes('greyjoy')) {
+    return 'House Greyjoy';
+  } else if (name.includes('martell')) {
+    return 'House Martell';
+  } else if (name.includes('arryn')) {
+    return 'House Arryn';
+  } else if (name.includes('frey')) {
+    return 'House Frey';
+  } else if (name.includes('tyrell')) {
+    return 'House Tyrell';
+  } else if (name.includes('tully')) {
+    return 'House Tully';
+  } else if (
+    name.includes('none') ||
+    name.includes('unknown') ||
+    name.includes('unkown')
+  ) {
+    return 'House Unknown';
+  } else {
+    // For all other names, capitalize the name given. As it is their last name most of the time
+    // aka they are their own house
+    return capitalizeHouseNames(name);
+  }
+}
 
 // url for the Thrones API
 const url = 'https://thronesapi.com/api/v2/Characters';
 
-const renderChart = () => {
+const renderChart = async () => {
+  const fetchApiData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  };
+  const data = await fetchApiData();
+  console.log(data);
+  const familyCounts = data.reduce((acc, char) => {
+    const correctedFamilyName = standardizeHouseName(char.family);
+    if (char.family) {
+      acc[correctedFamilyName] = (acc[correctedFamilyName] || 0) + 1;
+    }
+    return acc;
+  }, {});
+
+  const labels = Object.keys(familyCounts);
+  const familyData = Object.values(familyCounts);
   const donutChart = document.querySelector('.donut-chart');
 
   new Chart(donutChart, {
     type: 'doughnut',
     data: {
-      labels: ['label', 'label', 'label', 'label'],
+      labels: labels,
       datasets: [
         {
           label: 'My First Dataset',
-          data: [1, 12, 33, 5],
+          data: familyData,
           backgroundColor: backgroundColors,
           borderColor: borderColors,
           borderWidth: 1,
